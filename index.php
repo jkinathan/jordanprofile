88fe8ad8e735c9880dd7892b3380f6d1e9c6fb07
jkinathan <jkinathan@gmail.com>
2021-01-19 17:33:55 +0300

    part done on index page

--- index.php
+++ index.php
@@ -1,81 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-
-.topnav .icon {
-  display: none;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
-</style>
-</head>
-<body>
-
-<div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
+<?php include 'header.php';
+?>
+<div style="padding-left:16px">
+  <h2>WELCOME TO MY PROFILE PAGE</h2>
 </div>
 
-<div style="padding-left:16px">
-  <h2>Responsive Topnav Example</h2>
-  <p>Resize the browser window to see how it works.</p>
+<div class="row" style="background-color: lightyellow;">
+  <div class="column">
+    <div class="card">
+      <img src="assets/images/profile.jpeg" alt="Jordan" style="border-radius: 5px; max-height: 380px;width:100%;">
+      <p>
+      <button style="font-family: monospace;">
+          Experienced Junior Developer with a demonstrated history of working in the computer software industry. 
+        </button>
+      </p>
+    </div>
+  </div>
+  <div class="column card">
+    <h1>KINOBE JORDAN ATHANASIUS</h1>
+        <p class="title">Student Developer</p>
+        <p>Uganda Christian University</p>
+        <a href="#"><i class="fa fa-dribbble"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-facebook"></i></a>
+    <div id="London" class="tabcontent" style="border-radius: 8px;">
+      <h1>Course</h1>
+      <p>Bachelor of Science in Computer Science</p>
+    </div>
+
+    <div id="Paris" class="tabcontent" style="border-radius: 8px;">
+      <h1>Institution</h1>
+      <p>Uganda Christian University Mukono.</p>
+    </div>
+
+    <div id="Tokyo" class="tabcontent" style="border-radius: 8px;">
+      <h1>Year of Study</h1>
+      <p>Second Year First Semester</p>
+    </div>
+
+    <div id="Oslo" class="tabcontent" style="border-radius: 8px;">
+      <h1>Address</h1>
+      <p>Plot 56, Kiwafu,Circular road, Entebbe Uganda</p>
+    </div>
+
+    <button class="tablink" onclick="openCity('London', this, 'red')" id="defaultOpen">Course</button>
+    <button class="tablink" onclick="openCity('Paris', this, 'green')">Institution</button>
+    <button class="tablink" onclick="openCity('Tokyo', this, 'blue')">Year</button>
+    <button class="tablink" onclick="openCity('Oslo', this, 'orange')" style="border-radius: 0px; 0px; 0px; 8px;">Address</button>
+  </div>
 </div>
 
 <script>
@@ -87,7 +59,32 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-</script>
 
+function openCity(cityName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(cityName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+</script>
+<footer></footer>
 </body>
 </html>
---
+++ project.php
@@ -0,0 +1,35 @@
+<?php include 'header.php';
+?>
+<link rel="stylesheet" href="assets/css/project.css">
+<div style="padding-left:16px">
+  <h2>HERE ARE MY PROJECTS</h2>
+</div>
+
+<div class="bigdiv">
+    <div class="column card" style="max-width: 400px;">
+    <img src="assets/images/project1.png" alt="Avatar" style="width:100%">
+    <div class="container">
+        <h4><b>Tubayo</b></h4>
+        <p>Tours and Travel</p>
+        <p><button>Visit Website</button></p>
+    </div>
+    </div>
+
+    <div class="column card" style="max-width: 400px;">
+    <img src="assets/images/project2.png" alt="Avatar" style="width:100%">
+    <div class="container">
+        <h4><b>SolarMovies</b></h4>
+        <p>Movies application</p>
+        <p><button>Visit Website</button></p>
+    </div>
+    </div>
+
+    <div class="column card" style="max-width: 400px;">
+    <img src="assets/images/project4.png" alt="Avatar" style="width:100%">
+    <div class="container">
+        <h4><b>United Diesel</b></h4>
+        <p>Motor Hardware app</p>
+        <p><button>Visit Website</button></p>
+    </div>
+    </div>
+</div>
